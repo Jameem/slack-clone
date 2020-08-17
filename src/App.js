4ccac63d76b68824cbccfddf2fe9d10b8639e2ff
Jameem <jameemkurikkal.mp@gmail.com>
2020-08-17 09:00:26 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import "./App.css"
 import Header from "./components/Header"
 import Sidebar from "./components/Sidebar"
+import Chat from "./components/Chat"
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-      </div>
+      <Router>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   )
 }
