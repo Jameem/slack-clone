22b1ab4cdd88650db0b3ea873810632307d9f422
Jameem <jameemkurikkal.mp@gmail.com>
2020-08-18 09:37:31 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import React from "react"
+import React, { useState } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import "./App.css"
 import Header from "./components/Header"
 import Sidebar from "./components/Sidebar"
 import Chat from "./components/Chat"
+import Login from "./components/Login"
+import { useStateValue } from "./stateProvider"
 
 function App() {
+  const [{ user }, dispatch] = useStateValue()
+
   return (
     <div className="app">
       <Router>
-        <Header />
-        <div className="app__body">
-          <Sidebar />
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <div className="app__body">
+              <Sidebar />
 
-          <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <h1>Welcome</h1>
-            </Route>
-          </Switch>
-        </div>
+              <Switch>
+                <Route path="/room/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  <h1>Welcome</h1>
+                </Route>
+              </Switch>
+            </div>
+          </>
+        )}
       </Router>
     </div>
   )
