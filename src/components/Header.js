e15866a426da3d9e7cff5ef864e4b673b976c544
Jameem <jameemkurikkal.mp@gmail.com>
2020-08-18 09:35:25 +0530

    updated header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,21 +4,26 @@ import { Avatar } from "@material-ui/core"
 import AccessTimeIcon from "@material-ui/icons/AccessTime"
 import SearchIcon from "@material-ui/icons/Search"
 import HelpOutlineIcon from "@material-ui/icons/HelpOutline"
+import { useStateValue } from "../stateProvider"
 
 function Header() {
+  const [{ user }] = useStateValue()
+
+  console.log(user?.photoURL)
+
   return (
     <div className="header">
       <div className="header__left">
         <Avatar
           className="header__avatar"
-          //   alt={user?.displayName}
-          //   src={user?.phtotURL}
+          alt={user?.displayName}
+          src={user?.photoURL}
         />
         <AccessTimeIcon />
       </div>
       <div className="header__search">
-        <SearchIcon />
         <input placeholder="Search" />
+        <SearchIcon />
       </div>
       <div className="header__right">
         <HelpOutlineIcon />
